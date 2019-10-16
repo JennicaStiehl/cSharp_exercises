ccc94c333c7acc8fd5db483004b9a24ef71e87ad
JennicaStiehl <jennica.stiehl@gmail.com>
2019-10-16 16:24:22 -0600

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            const float pi = 3.14f;
+            Console.WriteLine(pi);
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+            Console.WriteLine(b);
+            var john = new Person();
+            Math.Calculator calculator = new Math.Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+            var numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            Console.WriteLine(numbers[1]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+
+            var names = new string[3] { "jack", "jan", "joe" };
+            Console.WriteLine(names[0]);
+        }
+    }
+
+}
---
+++ HelloWorld/Struct/rgbColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorld.Struct
+{
+    public struct rgbColor
+    {
+        //use when you need a light weight object, like a point with 2 coordinates
+        public int Red;
+        public int Green;
+        public int Blue;
+
+    }
+}
